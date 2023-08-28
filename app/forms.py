fea8843b696f698b4a6ff76fa22cc774ebcf5f04
unknown <code-for-student>
2023-08-28 15:48:26 +0600

    PasswordReset Funtionality Added

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,86 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    PasswordChangeForm,
+    PasswordResetForm,
+    SetPasswordForm
+)
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth import password_validation
+
 
 class UserRegistrationForm(UserCreationForm):
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = User
-        fields = ('username','email','password1','password2')
-        label = {'password1':'Password','password2':'Conform Password'}
-        widgets = {'username': forms.TextInput(attrs={'class':'form-control'})}
+        fields = ("username", "email", "password1", "password2")
+        label = {"password1": "Password", "password2": "Conform Password"}
+        widgets = {"username": forms.TextInput(attrs={"class": "form-control"})}
 
+
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label=_("Old Password"),
+        strip=False,
+        widget=forms.TextInput(
+            attrs={
+                "autocomplete": "current-password",
+                "autofocus": True,
+                "class": "form-control",
+            }
+        ),
+    )
+    new_password1 = forms.CharField(
+        label=_("New Password"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "class": "form-control",
+            }
+        ),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    new_password2 = forms.CharField(
+        label=_("Conform Password"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "class": "form-control",
+            }
+        ),
+    )
+
+
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={"autocomplete": "email", "autofocus": True, "class": "form-control"}
+        ),
+    )
+
+class MySetPasswordForm(SetPasswordForm):
+    
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password","autofocus":True,"class":"form-control"}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password","class":"form-control"}),
+    )
--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = 'images/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/profile/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
